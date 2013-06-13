f3163803aca1215025c535b872bf4227997456fb
Peter Mancini <pete@nectarineimp.com>
2013-06-13 17:53:55 -0500

    Added a ton of new functions. Almost done with final unified wave format.

--- src/char_wave/analysis.clj
+++ src/char_wave/analysis.clj
@@ -1,6 +1,37 @@
 (ns char-wave.analysis)
 
-(defn char-map-freq [stream-input]
-  (--> stream-input
-       read-line)
-  )
+(defn -sort-part-chars
+  "Takes the input an partitions every unique byte into mini collections."
+  [s]
+  (partition-by identity (sort s)))
+
+(defn -char-wave [coll]
+  "Takes a mini-collection of a unique byte and creates a vector of byte and count."
+  (vector (int (first coll)) (count coll)))
+
+(defn char-waveform
+  "Creates a sequence of vectors of unique byte and count. The output is the unique waveform of the input."
+  [s]
+  (map -char-wave (-sort-part-chars s)))
+
+(defn -wave-size
+  "Examines the waveform for element count and produces that value to be used in normalization"
+  [wave]
+  (reduce (fn [a b] (+ a (second b))) 0 wave))
+
+(defn -magnify
+  [x]
+  "Takes a normal x, divides by 1/10 and rounds the double to zero decimiles. This creates 201 possible bins of 0-200"
+  (int (roundx (* x 25) 0)))
+
+(defn -normalize-wf
+  "Creates a normalized wave form of vectors of byte and count over total samples."
+  [wave]
+  (let
+    [scale (-wave-size wave)]
+    (map #(vector (first %) (+ 1 (-magnify (/ (second %) scale)))) wave)))
+
+(defn roundx
+  "x= number to be rounded, n= precision, maximum precision of 10."
+  [x n]
+  (/ (Math/round (* (Math/pow 10.0 (if (> n 10) 10 n)) x)) (Math/pow 10.0 (if (> n 10) 10 n))))
--- src/char_wave/core.clj
+++ src/char_wave/core.clj
@@ -4,4 +4,6 @@
 (defn -main
   "Take stdin, test for semantic text, return nil if none found or all input if it is ok."
   [& args]
-  (println args))
+  ;(map (fn [filename] (with-open [rdr (clojure.java.io/reader filename)]) (char-wave.analysis/char-map-freq rdr)))
+  )
+
